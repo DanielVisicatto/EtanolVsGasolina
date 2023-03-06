9918a68d26193a37ff6ce383907adfb1ad065c73
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 19:58:26 -0300

    Programa dividido em duas Funcoes simples

--- EtanolVsGasolina/Program.cs
+++ EtanolVsGasolina/Program.cs
@@ -1,15 +1,25 @@
-﻿Console.Write("Digite o preço da Gasolina: ");
-float precoGasolina = float.Parse(Console.ReadLine());
+﻿float diferenca = 0, precoEtanol = 0, precoGasolina = 0;
+RecebeDados();
+CalculoCombustivel();
 
-Console.Write("Digite o preço do Etanol: ");
-float precoEtanol = float.Parse(Console.ReadLine());
-
-float diferenca = precoEtanol / precoGasolina;
-if (diferenca <= .72)
+void CalculoCombustivel()
 {
-    Console.WriteLine("Use Etanol");
+    float diferenca = precoEtanol / precoGasolina;
+    if (diferenca <= .72)
+    {
+        Console.WriteLine("Use Etanol");
+    }
+    else
+    {
+        Console.WriteLine("Use Gasolina");
+    }
 }
-else
+void RecebeDados()
 {
-    Console.WriteLine("Use Gasolina");
+    Console.Write("Digite o preço da Gasolina: ");
+    float precoGasolina = float.Parse(Console.ReadLine());
+
+    Console.Write("Digite o preço do Etanol: ");
+    float precoEtanol = float.Parse(Console.ReadLine());
+
 }
