5e64751226942957b4481e386588877113292ba3
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 19:46:58 -0300

    Add project files.

---
+++ EtanolVsGasolina/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite o preço da Gasolina: ");
+float precoGasolina = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o preço do Etanol: ");
+float precoEtanol = float.Parse(Console.ReadLine());
+
+float diferenca = precoEtanol / precoGasolina;
+if (diferenca <= .72)
+{
+    Console.WriteLine("Use Etanol");
+}
+else
+{
+    Console.WriteLine("Use Gasolina");
+}
